67ff07c0fd439aca77a652f08c0d7900c94f0552
Dileep <dileepboorla@gmail.com>
2023-08-12 15:45:48 +0530

    files added

---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: "https://i.postimg.cc/9F68xYBV/todo.png",
+    title: "TO-DO list",
+    link: "",
+    git: "",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/nLDTJbRW/tailor.png',
+    title: "Tailor Shop Order Management",
+    link: "",
+    git: "",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/66789rCD/newcommers.png',
+    title: "Empowering New commer's",
+    link: "",
+    git: "",
+  }
+  
+];
